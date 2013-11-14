c9e9672664fc88752de98e977f818f66874520b0
CJ Sheets <sheets@g.harvard.edu>
2013-11-13 22:34:51 -0600

    add collapsible nvaigation, and sticky footer

--- index.php
+++ index.php
@@ -15,6 +15,40 @@
 	<![endif]-->
 </head>
 <body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+			
+				<!-- button displays when on mobile devices -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				
+				<a href="#" class="navbar-brand">Web Services Calculator</a>
+			</div>
+			<div class="collapse navbar-collapse">
+				
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">Preview</a></li>
+					<li><a href="#">Print</a></li>
+					<li><a href="#">Reset</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+	<!-- footer -->
+	<div class="navbar navbar-default navbar-fixed-bottom">
+	
+		<div class="container">
+			<p class="navbar-text pull-right">Application Developed by CJ Sheets</p>
+		</div>
+	
+	</div>
 	
 	
 	
