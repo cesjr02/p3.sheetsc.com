bf2e14211b106fa0b7f5af9d6fe55e17c63460f5
CJ Sheets <sheets@g.harvard.edu>
2013-11-25 12:17:00 -0600

    moved form tag to enclose entire form

--- index.php
+++ index.php
@@ -55,198 +55,219 @@
 	
 	
 	<!-- form for client information -->
-	<div class='container'>
-		<div class='row'>
-			<div class='col-md-12'>			
-				<h2 class='h2' >Client Information</h2>
-				<hr>
-				<p class='error'>* required field</p>
-					<form role='form'>
-						<div class='form-group'>
-							<label>Client Name<span class="error"> *</span></label>
-							
-							<input class='form-control input-lg' placeholder='Enter name' required>
-							
-						</div>
-						
-						<div class='form-group'>
-							<label>Project Name<span class="error"> *</span></label>
-							<input class='form-control input-lg' placeholder='Enter project name' required>
-						</div>
-						
-						<div class='form-group'>
-							<label>Phone</label>
-							<input type='tel' class='form-control input-lg' placeholder='Enter phone number'>
-						</div>
-						
-						<div class='form-group'>
-							<label>Address</label>
-							<input class='form-control input-lg' placeholder='Enter address'>
-						</div>
-						
-						<div class='form-group'>
-							<label>Date<span class="error"> *</span></label>
-							<input type='date' class='form-control input-lg' required>
-						</div>	
-						
-						<div class='form-group'>
-							<label>Budget<span class="error"> *</span></label>
-							<input class='form-control input-lg' name='budget' placeholder='Enter your budget' required>
-						</div>	
-				
-					</form>
-				
-			</div><!-- end of col -->	
-		</div><!-- end of row -->
-	</div><!-- end of container -->
-
-			
-	<!-- frontend options -->		
-	<div class='container'>
-		<div class='row'>
-			<div class='col-md-6'>
-				<h2 class='h2'>Design & Front-end</h2>
-				<hr>
-				<h3 class='h3'>Basics</h3>
-					<div class='radio'>
-						<label>Update or modify existing site - 3 hrs</label>
-						<input type='radio' name='options' id='optionsRadios1' value='3'>
-						
-					</div>
-					
-					<div class='radio'>
-						<label>Has template for customization - 6 hrs</label>
-						<input type='radio' name='options' id='optionsRadios2' value='6'>
-					</div>
-					
-					<div class='radio'>
-						<label>Needs template for customization - 10 hrs</label>
-						<input type='radio' name='options' id='optionsRadios3' value='10'>
-					</div>
-					
-					<div class='radio'>
-						<label>Build Custom Site - 20 hrs</label>
-						<input type='radio' name='options' id='optionsRadios4' value='20'>
-					</div>
-					
-				
-					<h3 class='h3'>Interactive Elements</h3>
-					
-					<div class='checkbox'>
-						<label data-toggle='popover' title='Image Gallery' data-content='Include a scrolling image gallery to your site. Features smooth animations, user controls, and optional captions' data-placement='top' data-trigger='hover'>Image Gallery - 4 hrs</label>
-						<input type='checkbox' name='options' value='4'>
-					</div>
-					
-					<div class='checkbox'>
-						<label data-toggle='popover' title='Animated Navigation Menu' data-content='Fluid animations for navigation menus and dropdown submenus' data-placement='right' data-trigger='hover'>Animated Navigation Menu - 3 hrs</label>
-						<input type='checkbox' name='options' value='3'>
-					</div>
-					
-					<div class='checkbox'>
-						<label data-toggle='popover' title='Advertisement Slider' data-content='Display your latest promotions or advertisements in a scrolling carousel' data-placement='bottom' data-trigger='hover'>Advertisement Slider - 6 hrs</label>
-						<input type='checkbox' name='options' value='6'>
-					</div>
-					
-					
-					
-					<h3 class='h3'>Add Ons</h3>
-					
-					<div class='checkbox'>
-						<label data-toggle='popover' title='Responsive Design' data-content='A web design approach aimed at building a site that provides optimal viewing experience across a wide range of devices' data-placement='top' data-trigger='hover'>Responsive Design - 12 hrs</label>
-						<input type='checkbox' name='options' value='12'>
-					</div>
-					
-					<div class='checkbox'>
-						<label>jQuery Mobile Site - 40 hrs</label>
-						<input type='checkbox' name='options' value='40'>
-					</div>
-					
-					<div class='checkbox'>
-						<label>Social Media Integration - 6 hrs</label>
-						<input type='checkbox' name='options' value='6'>
-					</div>
-					
-					<div class='checkbox'>
-						<label data-toggle='popover' title='Logo Design' data-content='A custom designed logo will add a professional look to your website' data-placement='right' data-trigger='hover'>Logo Design - 4 hrs</label>
-						<input type='checkbox' name='options' value='4'>
-					</div>
-					
-					<div class='checkbox'>
-						<label data-toggle='popover' title='Custom Branding' data-content='Branding will add a personalized brand identity that your customers will recognize' data-placement='right' data-trigger='hover'>Custom Branding - 6 hrs</label>
-						<input type='checkbox' name='options' value='6'>
-					</div>		
-			</div>
-			
-			
-			<!-- backend development options -->
-			<div class='col-md-6'>
-				<h2 class='h2'>Back-end Development</h2>
-				<hr>
-				<h3 class='h3'>Under the Hood</h3>
-					
-					<div class='checkbox'>
-						<label>Content Management System - 6 hrs</label>
-						<input type='checkbox' name='options' value='6'>
-					</div>
-					
-					<div class='checkbox'>
-						<label>Admin Login - 5 hrs</label>
-						<input type='checkbox' name='options' value='5'>
-					</div>
-					
-					<div class='checkbox'>
-						<label>Contact Form - 3 hrs</label>
-						<input type='checkbox' name='options' value='3'>
-					</div>
-					
-					<div class='checkbox'>
-						<label>Email Auto Responders - 2 hrs</label>
-						<input type='checkbox' name='options' value='2'>
-					</div>
-					
-					<div class='checkbox'>
-						<label>Custom Blog - 6 hrs</label>
-						<input type='checkbox' name='options' value='6'>
-					</div>
-					
-				
-					<h3 class='h3'>Search Engine Optimization</h3>
-					
-					<div class='checkbox'>
-						<label>Meta Tag Keyword Optimization  - 5 hrs</label>
-						<input type='checkbox' name='options' value='5'>
-					</div>
-					
-					<div class='checkbox'>
-						<label>Link Building - 6 hrs</label>
-						<input type='checkbox' name='options' value='6'>
-					</div>
-					
-					<div class='checkbox'>
-						<label>User Traffic Reports - 1 hr</label>
-						<input type='checkbox' name='options' value='1'>
-					</div>
-					
-					<h2 class='h2'>Total</h2>
+	<form role='form' id='form'>
+		<div class='container'>
+			<div class='row'>
+				<div class='col-md-12'>			
+					<h2 class='h2' >Client Information</h2>
 					<hr>
-					<p class='totals'>Budget: $<span class='budget'>0</span></p>
-					<p class='totals'>Total Hours: <span class='total-hours'>0</span></p>
-					<p class='totals'>Total Cost: $<span class='total-cost'>0.00</span></p>
-			
-			</div><!-- end of col -->
-		</div><!-- end of row -->
-	</div><!-- end of container -->
+					<p class='error'>* required field</p>
+							<div class='form-group'>
+								<label>Client Name<span class="error"> *</span></label>
+								
+								<input class='form-control input-lg' placeholder='Enter name' required>
+								
+							</div>
+							
+							<div class='form-group'>
+								<label>Project Name<span class="error"> *</span></label>
+								<input class='form-control input-lg' placeholder='Enter project name' required>
+							</div>
+							
+							<div class='form-group'>
+								<label>Phone</label>
+								<input type='tel' class='form-control input-lg' placeholder='Enter phone number'>
+							</div>
+							
+							<div class='form-group'>
+								<label>Address</label>
+								<input class='form-control input-lg' placeholder='Enter address'>
+							</div>
+							
+							<div class='form-group'>
+								<label>Date<span class="error"> *</span></label>
+								<input type='date' class='form-control input-lg' required>
+							</div>	
+							
+							<div class='form-group'>
+								<label>Budget<span class="error"> *</span></label>
+								<input class='form-control input-lg' name='budget' placeholder='Enter your budget' maxlength="4" required>
+							</div>	
+							
+							
+					
+				</div><!-- end of col -->	
+			</div><!-- end of row -->
+		</div><!-- end of container -->
 	
+				
+		<!-- frontend options -->		
+		<div class='container'>
+			<div class='row'>
+				<div class='col-md-6'>
+					<h2 class='h2'>Design & Front-end</h2>
+					<hr>
+					<h3 class='h3'>Basics</h3>
+						<div class='radio'>
+							<label>Update or modify existing site - 3 hrs</label>
+							<input type='radio' name='options' id='optionsRadios1' value='3'>
+							
+						</div>
+						
+						<div class='radio'>
+							<label>Has template for customization - 6 hrs</label>
+							<input type='radio' name='options' id='optionsRadios2' value='6'>
+						</div>
+						
+						<div class='radio'>
+							<label>Needs template for customization - 10 hrs</label>
+							<input type='radio' name='options' id='optionsRadios3' value='10'>
+						</div>
+						
+						<div class='radio'>
+							<label>Build Custom Site - 20 hrs</label>
+							<input type='radio' name='options' id='optionsRadios4' value='20'>
+						</div>
+						
+					
+						<h3 class='h3'>Interactive Elements</h3>
+						
+						<div class='checkbox'>
+							<label data-toggle='popover' title='Image Gallery' data-content='Include a scrolling image gallery to your site. Features smooth animations, user controls, and optional captions' data-placement='top' data-trigger='hover'>Image Gallery - 4 hrs</label>
+							<input type='checkbox' name='options' value='4'>
+						</div>
+						
+						<div class='checkbox'>
+							<label data-toggle='popover' title='Animated Navigation Menu' data-content='Fluid animations for navigation menus and dropdown submenus' data-placement='right' data-trigger='hover'>Animated Navigation Menu - 3 hrs</label>
+							<input type='checkbox' name='options' value='3'>
+						</div>
+						
+						<div class='checkbox'>
+							<label data-toggle='popover' title='Advertisement Slider' data-content='Display your latest promotions or advertisements in a scrolling carousel' data-placement='bottom' data-trigger='hover'>Advertisement Slider - 6 hrs</label>
+							<input type='checkbox' name='options' value='6'>
+						</div>
+						
+						
+						
+						<h3 class='h3'>Add Ons</h3>
+						
+						<div class='checkbox'>
+							<label data-toggle='popover' title='Responsive Design' data-content='A web design approach aimed at building a site that provides optimal viewing experience across a wide range of devices' data-placement='top' data-trigger='hover'>Responsive Design - 12 hrs</label>
+							<input type='checkbox' name='options' value='12'>
+						</div>
+						
+						<div class='checkbox'>
+							<label>jQuery Mobile Site - 40 hrs</label>
+							<input type='checkbox' name='options' value='40'>
+						</div>
+						
+						<div class='checkbox'>
+							<label>Social Media Integration - 6 hrs</label>
+							<input type='checkbox' name='options' value='6'>
+						</div>
+						
+						<div class='checkbox'>
+							<label data-toggle='popover' title='Logo Design' data-content='A custom designed logo will add a professional look to your website' data-placement='right' data-trigger='hover'>Logo Design - 4 hrs</label>
+							<input type='checkbox' name='options' value='4'>
+						</div>
+						
+						<div class='checkbox'>
+							<label data-toggle='popover' title='Custom Branding' data-content='Branding will add a personalized brand identity that your customers will recognize' data-placement='right' data-trigger='hover'>Custom Branding - 6 hrs</label>
+							<input type='checkbox' name='options' value='6'>
+						</div>		
+				</div>
+				
+				
+				<!-- backend development options -->
+				<div class='col-md-6'>
+					<h2 class='h2'>Back-end Development</h2>
+					<hr>
+					<h3 class='h3'>Under the Hood</h3>
+						
+						<div class='checkbox'>
+							<label>Content Management System - 6 hrs</label>
+							<input type='checkbox' name='options' value='6'>
+						</div>
+						
+						<div class='checkbox'>
+							<label>Admin Login - 5 hrs</label>
+							<input type='checkbox' name='options' value='5'>
+						</div>
+						
+						<div class='checkbox'>
+							<label>Contact Form - 3 hrs</label>
+							<input type='checkbox' name='options' value='3'>
+						</div>
+						
+						<div class='checkbox'>
+							<label>Email Auto Responders - 2 hrs</label>
+							<input type='checkbox' name='options' value='2'>
+						</div>
+						
+						<div class='checkbox'>
+							<label>Custom Blog - 6 hrs</label>
+							<input type='checkbox' name='options' value='6'>
+						</div>
+						
+					
+						<h3 class='h3'>Search Engine Optimization</h3>
+						
+						<div class='checkbox'>
+							<label>Meta Tag Keyword Optimization  - 5 hrs</label>
+							<input type='checkbox' name='options' value='5'>
+						</div>
+						
+						<div class='checkbox'>
+							<label>Link Building - 6 hrs</label>
+							<input type='checkbox' name='options' value='6'>
+						</div>
+						
+						<div class='checkbox'>
+							<label>User Traffic Reports - 1 hr</label>
+							<input type='checkbox' name='options' value='1'>
+						</div>
+						
+						<h2 class='h2'>Total</h2>
+						<hr>
+						<p class='totals'>Budget: $<span class='budget'>0</span></p>
+						<p class='totals'>Total Hours: <span class='total-hours'>0</span></p>
+						<p class='totals'>Total Cost: $<span class='total-cost'>0.00</span></p>
+				
+				</div><!-- end of col -->
+			</div><!-- end of row -->
+		</div><!-- end of container -->
+		
+		
+		<!-- summary of work or contractual terms -->
+		<div class='container'>
+			<h2 class='h2'>Agreement</h2>
+			<hr>
+				<div class='form-group'>
+					<label>(for developer use only)</label>
+					<textarea class='form-control' rows='6'></textarea>
+				</div>
+		</div><!-- end of container -->
+		
+		<!-- modal for form reset -->
+		<div class='modal fade' id='reset'>
+			<div class='modal-dialog'>
+				<div class='modal-content'>
+					<div class='modal-header'>
+						<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
+						<h4 class='modal-title'>Reset Form</h4>
+					</div>
+						<div class='modal-body'>
+						<p>Are you sure you want to reset this form?</p>
+					</div>
+						<div class='modal-footer'>
+						<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+						<button type='button' class='btn btn-primary' id='reset2'>Reset</button>
+					</div>
+				</div><!-- /.modal-content -->
+			</div><!-- /.modal-dialog -->
+		</div><!-- /.modal -->
 	
-	<!-- summary of work or contractual terms -->
-	<div class='container'>
-		<h2 class='h2'>Agreement</h2>
-		<hr>
-			<div class='form-group'>
-				<label>(for developer use only)</label>
-				<textarea class='form-control' rows='6'></textarea>
-			</div>
-	</div><!-- end of container -->
+	</form>
 	
 
 	<!-- footer -->
@@ -268,24 +289,7 @@
 	</div>
 	
 	
-	<!-- modal for form reset -->
-	<div class='modal fade' id='reset'>
-		<div class='modal-dialog'>
-			<div class='modal-content'>
-				<div class='modal-header'>
-					<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button>
-					<h4 class='modal-title'>Reset Form</h4>
-				</div>
-					<div class='modal-body'>
-					<p>Are you sure you want to reset this form?</p>
-				</div>
-					<div class='modal-footer'>
-					<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
-					<button type='button' class='btn btn-primary' id='reset'>Reset</button>
-				</div>
-			</div><!-- /.modal-content -->
-		</div><!-- /.modal-dialog -->
-	</div><!-- /.modal -->
+
 		
 	
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
@@ -294,6 +298,7 @@
 	<script src='js/bootstrap.min.js'></script>
 	<!-- include external js -->
 	<script src='js/bidcalculator.js'></script>	
+	<script src='js/formreset.js'></script>
 	
 </body>
 </html>
