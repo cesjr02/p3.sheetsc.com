9ee7bc828f3dbda13d42e5c64aa132bb9facea25
CJ Sheets <sheets@g.harvard.edu>
2013-11-25 12:47:08 -0600

    clean up reset function

--- js/formreset.js
+++ js/formreset.js
@@ -1,16 +1,10 @@
 $(function() {
     $('#reset2').click(function() {
         $('form')[0].reset();
+        $('.budget')[0].reset();
+        $('.total-cost')[0].reset();
+        $('.total-hours')[0].reset();
+        
     });
     
-    $('#reset3').click(function() {
-        $('#form').each(function(i, ctrl) {
-            ctrl.checked = ctrl.defaultChecked;
-            ctrl.value = ctrl.defaultValue;
-        });
-    });
-    
-    $('#reset4').click(function() {
-        document.getElementById('form').reset();
-    });
 });
