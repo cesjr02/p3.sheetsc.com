0f9aa65cb92a37b755b033dde3dcb15f87418249
CJ Sheets <sheets@g.harvard.edu>
2013-11-25 18:06:32 -0600

    add code to reset totals to 0.00

--- js/formreset.js
+++ js/formreset.js
@@ -1,9 +1,9 @@
 $(function() {
     $('#reset2').click(function() {
         $('form')[0].reset();
-        $('.budget')[0].reset();
-        $('.total-cost')[0].reset();
-        $('.total-hours')[0].reset();
+        $('.total-cost').html("0.00");
+        $('.total-hours').html("0");
+        $('.budget').html("0.00");
         
     });
     
