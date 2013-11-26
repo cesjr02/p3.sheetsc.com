32618d63604381bca827a70f6bb6ba8a573cad39
CJ Sheets <sheets@g.harvard.edu>
2013-11-25 18:13:23 -0600

    comments and .css
    Add comments and code to change .total-cost back to black if red or
    green.

--- js/formreset.js
+++ js/formreset.js
@@ -1,9 +1,16 @@
 $(function() {
     $('#reset2').click(function() {
+    
+    	// reset form inputs
         $('form')[0].reset();
+        
+        // zero out totals when reset button is pressed
         $('.total-cost').html("0.00");
         $('.total-hours').html("0");
         $('.budget').html("0.00");
+        
+        // change color style back to black if red or green
+        $('.total-cost').css('color', 'black');
         
     });
     
